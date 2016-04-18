2f00a3157fb55335babe96f4418ea6d013ace51b
julfelder <julfelder@wustl.edu>
2016-04-18 18:30:41 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -4,12 +4,21 @@ var order = '';
 
 // build a function to ask questions
 
-function question() {
-  // your code goes here
+function question(questionText, answerText, alt) {
+ var answer1 = prompt(questionText);
+ if(answer1.toUpperCase()=== answerText){
+ order += answerText + " + "}
+ else{
+   order+= alt+ " + "
+ }
 }
 
 // call the QUESTION function with the order options
-question();
+question("Brown or white rice?", "BROWN", "WHITE");
+question("Brown or black beans?", "BROWN", "BLACK");
+question("Guac or no guac?", "GUAC", "NO GUAC");
+question("Veggies or no veggies?", "VEGGIES", "NO VEGGIES");
+question("Salsa or no salsa?", "SALSA", "NO SALSA");
 
 // output their order
 document.write("<p>You ordered: " + order + "<p>");
